fb6e1583690854054064d4ad6ebae8c38f899d9f
Alexey Alenich <pnsrc@ya.ru>
2026-02-27 11:45:23 +0600

    Use WIN32 subsystem and set app icon

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <QApplication>
 #include <QMainWindow>
+#include <QIcon>
 
 #include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/assets/logo.png"));
     QMainWindow *w = createMainWindow();
     w->show();
     return app.exec();
